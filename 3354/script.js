bee4dd0e3c7c50597b6f2fcc90e008bc3eb71421
V-DSGN <designers@loyaltyplant.com>
2026-05-22 18:13:46 +0400

    Update script.js

--- 3354/script.js
+++ 3354/script.js
@@ -1,39 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const BASE_PATH = 'https://cdn.jsdelivr.net/gh/V-DSGN/memory-game@main/3354/
     const cardsArray = [
   {
     id: 1,
-    img: './img/card-1-1.png', crustName: "Margherita"
+    img: 'card-1-1.png', crustName: "Margherita"
   },
   {
     id: 1,
-    img: './img/card-1-2.png', crustName: "Five Cheeses"
+    img: 'card-1-2.png', crustName: "Five Cheeses"
   },
 
   {
     id: 2,
-    img: './img/card-2-1.png', crustName: "Napoletana"
+    img: 'card-2-1.png', crustName: "Napoletana"
   },
   {
     id: 2,
-    img: './img/card-2-2.png', crustName: "Tonno & Cipollo"
+    img: 'card-2-2.png', crustName: "Tonno & Cipollo"
   },
 
   {
     id: 3,
-    img: './img/card-3-1.png', crustName: "Pepperoni" 
+    img: 'card-3-1.png', crustName: "Pepperoni" 
   },
   {
     id: 3,
-    img: './img/card-3-2.png', crustName: "Ham & Pineapple"
+    img: 'card-3-2.png', crustName: "Ham & Pineapple"
   },
 
   {
     id: 4,
-    img: './img/card-4-1.png', crustName: "Noduja (Vegan)"
+    img: 'card-4-1.png', crustName: "Noduja (Vegan)"
   },
   {
     id: 4,
-    img: './img/card-4-2.png', crustName: "Marinara"
+    img: 'card-4-2.png', crustName: "Marinara"
   }
     ];
     const gameContainer = document.getElementById('game-container');
@@ -73,11 +74,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function createCard(card) {
         const cardElement = document.createElement('div');
         cardElement.classList.add('card');
-        cardElement.dataset.id = card.id;
+        cardElement.dataset.id = BASE_PATH + card.id;
         cardElement.dataset.crust = card.crustName;
 
         const frontFace = document.createElement('img');
-        frontFace.src = './img/logo.png';  // Adjust path if necessary
+        frontFace.src = BASE_PATH + 'logo.png';  // Adjust path if necessary
         frontFace.className = 'front-face';
 
         const backFace = document.createElement('img');
